ccd11035d6b74789c1016a129196d3287a09228c
Saif Al-Naib <salnaib@ryerson.ca>
2013-06-10 20:36:42 -0400

    login

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include "socialmedia_oauth_connect.php";
+$oauth = new socialmedia_oauth_connect();
+$oauth->provider="MeetUp";
+$oauth->client_id = "elimmsk4tfqtc7b9il0vk5u3d7";
+$oauth->client_secret = "amq5s5799jnv5bdrt65ofg4mtq";
+$oauth->redirect_uri  ="http://radiant-anchorage-6507.herokuapp.com/messenger.php";
+
+$oauth->scope="basic";
+
+$oauth->Initialize();
+
+$code = ($_REQUEST["code"]) ?  ($_REQUEST["code"]) : "";
+
+if(empty($code)) {
+	$oauth->Authorize();
+}else{
+	$oauth->code = $code;
+#	$oauth->getAccessToken();
+	$getData = json_decode($oauth->getUserProfile());
+	$oauth->debugJson($getData);
+	print "Name :".$getData->data->full_name;
+
+}
+?>
