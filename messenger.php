f2aa1a9fb7f3dcbb97f07d8804b7edab2475f2b7
Saif Al-Naib <salnaib@ryerson.ca>
2013-06-10 21:22:17 -0400

    login

--- messenger.php
+++ messenger.php
@@ -1,7 +1,5 @@
 <?php
 
-echo $_GET["code"];
-
 $url = 'https://secure.meetup.com/oauth2/access?client_id=elimmsk4tfqtc7b9il0vk5u3d7&client_secret=amq5s5799jnv5bdrt65ofg4mtq&grant_type=authorization_code&redirect_uri=http://radiant-anchorage-6507.herokuapp.com/messenger.php&code='.$_GET["code"];
 
 $ch=curl_init($url);
@@ -10,6 +8,8 @@ curl_setopt($ch,CURLOPT_POST, 5);
 $r=curl_exec($ch);
 curl_close($ch);
 
-echo $r
+$_SESSION['accesstoken'] = json_decode($r)['access_token'];
+
+echo $_SESSION['accesstoken'];
 
 ?>
