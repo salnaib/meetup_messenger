5f2f6d6bb4dc0fbf256e31eab18b09c3fd457fd1
Saif Al-Naib <salnaib@ryerson.ca>
2013-06-11 21:37:46 -0400

    messaging

--- sendmessage.php
+++ sendmessage.php
@@ -5,7 +5,7 @@ session_start();
 //check if you have curl loaded
 if(!function_exists("curl_init")) die("cURL extension is not installed");
 
-$url = 'https://api.meetup.com/2/message?access_token='.$_SESSION['accesstoken'].'&member_id='.$_POST["memberid"].'group_id='.$_POST["groupid"].'&message='.$_POST["message"].'$subject='.$_POST["subject"];
+$url = 'https://api.meetup.com/2/message?access_token='.$_SESSION['accesstoken'].'&member_id='.$_POST["memberid"].'&group_id='.$_POST["groupid"].'&message='.$_POST["message"].'$subject='.$_POST["subject"];
 
 $ch=curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
